c585d6f95e9ed9d4f3b3d9aec217545381fd6e6f
cheesebob12 <134903358+cheesebob12@users.noreply.github.com>
2025-05-20 20:04:35 +0000

    A little late

--- src/main/java/edu/miracosta/cs112/lotaria/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lotaria/HelloApplication.java
@@ -1,22 +1,76 @@
 package edu.miracosta.cs112.lotaria;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import java.util.Random;
 
 public class HelloApplication extends Application {
     private static final LoteriaCard[] LOTERIA_CARDS = {
             new LoteriaCard("Las matematicas", "1.png", 1),
             new LoteriaCard("Las ciencias", "2.png", 2),
             new LoteriaCard("La Tecnología", "8.png", 8),
-            new LoteriaCard("La ingeniería", "9.png", 9),
+            new LoteriaCard("La ingeniería", "9.png", 9)
     };
+
+    private boolean[] drawnCards = new boolean[LOTERIA_CARDS.length];
+    private int cardsDrawn = 0;
+    private final Random rand = new Random();
 
     @Override
     public void start(Stage stage) {
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 320, 240);
+        VBox root = new VBox(15);
+        root.setAlignment(Pos.CENTER);
+        root.setStyle("-fx-padding: 20px; -fx-background-color: #fdf6e3;");
+
+        Label titleLabel = new Label("EChALE STEM Loteria!");
+        titleLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
+
+        ImageView cardImageView = new ImageView(LOTERIA_CARDS[0].getImage());
+        cardImageView.setFitWidth(200);
+        cardImageView.setFitHeight(300);
+        cardImageView.setPreserveRatio(true);
+
+        Label messageLabel = new Label("Click the button to draw a card!");
+
+        Button drawCardButton = new Button("Draw Random Card");
+
+        ProgressBar gameProgressBar = new ProgressBar(0.0);
+        gameProgressBar.setPrefWidth(200);
+
+        drawCardButton.setOnAction(event -> {
+            if (cardsDrawn >= LOTERIA_CARDS.length) return;
+
+            int index;
+            do {
+                index = rand.nextInt(LOTERIA_CARDS.length);
+            } while (drawnCards[index]);
+
+            drawnCards[index] = true;
+            cardsDrawn++;
+
+            LoteriaCard card = LOTERIA_CARDS[index];
+            cardImageView.setImage(card.getImage());
+            messageLabel.setText("You drew: " + card.getCardName());
+
+            gameProgressBar.setProgress((double) cardsDrawn / LOTERIA_CARDS.length);
+
+            if (cardsDrawn == LOTERIA_CARDS.length) {
+                cardImageView.setImage(new LoteriaCard().getImage()); // Echale logo
+                messageLabel.setText("GAME OVER. No more cards! Exit and run again to reset ^_^");
+                gameProgressBar.setStyle("-fx-accent: red;");
+                drawCardButton.setDisable(true);
+            }
+        });
+
+        root.getChildren().addAll(titleLabel, cardImageView, messageLabel, drawCardButton, gameProgressBar);
+
+        Scene scene = new Scene(root, 350, 500);
         stage.setTitle("Loteria!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/edu/miracosta/cs112/lotaria/LoteriaCard.java
+++ src/main/java/edu/miracosta/cs112/lotaria/LoteriaCard.java
@@ -1,59 +1,31 @@
 package edu.miracosta.cs112.lotaria;
 
 import javafx.scene.image.Image;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-/* UML CLASS DIAGRAM:
------------------------------------------
-LoteriaCard
------------------------------------------
-- cardName : String
-- imageName : String
-- cardNum : int
------------------------------------------
-+ LoteriaCard(cardName : String, imageName : String, cardNum : int)
-+ LoteriaCard(original : LoteriaCard)
-+ setAll(cardName : String, imageName : String, cardNum : int)
-+ getCardName() : String
-+ setCardName(cardName : String) : void
-+ getImageName() : String
-+ getImage() : Image
-+ setImageName(imageName : String) : void
-+ getCardNum() : int
-+ setCardNum(carNum : int) : void
-+ toString() : String
------------------------------------------
-*/
-
 public class LoteriaCard {
-    // INSTANCE VARIABLES
     private String cardName = "";
     private String imageName = "";
     private int cardNum = 0;
 
-    //Constructor
     public LoteriaCard(String cardName, String imageName, int cardNum) {
         this.setAll(cardName, imageName, cardNum);
     }
 
-    //Copy constructor with error checking for null values
     public LoteriaCard(LoteriaCard original) {
         if (original != null) {
             this.setAll(original.cardName, original.imageName, original.cardNum);
         } else {
-            System.out.println("ERROR: trying to copy NULL object. Exiting program...");
+            System.err.println("ERROR: trying to copy NULL object. Exiting...");
             System.exit(1);
         }
     }
 
-    //default constructor creates false loteria card (like Joker cards in a 52 card standard deck)
     public LoteriaCard() {
         this("EChALE STEM Logo", "0.png", 0);
     }
 
-    // get/set methods
     public void setAll(String cardName, String imageName, int cardNum) {
         this.setCardName(cardName);
         this.setImageName(imageName);
@@ -72,21 +44,13 @@ public class LoteriaCard {
         return this.imageName;
     }
 
-    /**
-     * Gets Image object for this card by reading it from the appropriate location in a replit project
-     *
-     * @return returns Image object that can be used to display it in an ImageView in GUI
-     **/
     public Image getImage() {
-        FileInputStream input = null;
         try {
-            input = new FileInputStream("file:./resources/" + this.imageName);
+            return new Image(new FileInputStream("resources/" + this.imageName));
         } catch (FileNotFoundException e) {
-            //e.printStackTrace();
-            System.err.println("ERROR: could not open file.");
-            System.exit(0);
+            System.err.println("ERROR: Could not load image " + this.imageName);
+            return null;
         }
-        return new Image(input);
     }
 
     public void setImageName(String imageName) {
@@ -101,13 +65,11 @@ public class LoteriaCard {
         this.cardNum = cardNum;
     }
 
-    //toString method
     @Override
     public String toString() {
-        return "Loteria Card #" + this.cardNum + ": " + this.cardName + "(" + this.imageName + ")";
+        return "Loteria Card #" + this.cardNum + ": " + this.cardName + " (" + this.imageName + ")";
     }
 
-    //Equals method
     @Override
     public boolean equals(Object o) {
         if (o instanceof LoteriaCard other) {
